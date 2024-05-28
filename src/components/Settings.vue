4ab918a09a578e2ebc81da336ff60c52e585854e
Tobias Rosenberg <tobias.ros@outlook.de>
2024-05-28 23:43:42 +0200

    added dropdowns for all settings + padding

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -17,14 +17,45 @@ import {RouterLink} from "vue-router";
             Worttyp
           </button>
           <ul class="dropdown-menu">
-            <li><button class="dropdown-item" type="button">Action</button></li>
-            <li><button class="dropdown-item" type="button">Another action</button></li>
-            <li><button class="dropdown-item" type="button">Something else here</button></li>
+            <li><button class="dropdown-item" type="button">Substantive</button></li>
+            <li><button class="dropdown-item" type="button">Verben</button></li>
+            <li><button class="dropdown-item" type="button">Adjektive</button></li>
           </ul>
         </div>
       </div>
-      <div class="col">Modus</div>
-      <div class="col">Anzahl/Zeit</div>
+      <div class="col">
+        <div class="dropdown">
+          <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+          >
+            Modus
+          </button>
+          <ul class="dropdown-menu">
+            <li><button class="dropdown-item" type="button">Wortzahl</button></li>
+            <li><button class="dropdown-item" type="button">Zeit</button></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col">
+        <div class="dropdown">
+          <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+          >
+            Anzahl/Zeit
+          </button>
+          <ul class="dropdown-menu">
+            <li><button class="dropdown-item" type="button">10 </button></li>
+            <li><button class="dropdown-item" type="button">20</button></li>
+            <li><button class="dropdown-item" type="button">50</button></li>
+          </ul>
+        </div>
+      </div>
       <div class="col">
         <RouterLink to="/scores" class="button">
           Scores
@@ -35,5 +66,7 @@ import {RouterLink} from "vue-router";
 </template>
 
 <style scoped>
-
+.container {
+  padding: 20px;
+}
 </style>
